840203357ded748efec85e2734beb13b3f96ce38
LTKSK <ltksk0714@gmail.com>
2023-02-24 17:12:30 +0900

    a17

--- tessoku-book/src/bin/a17.rs
+++ tessoku-book/src/bin/a17.rs
@@ -1,3 +1,37 @@
+use itertools::Itertools;
+use proconio::input;
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n-1],
+        b: [usize; n-2],
+    }
+
+    let mut res = vec![];
+
+    res.push(0);
+    res.push(a[0]);
+    for i in 2..n {
+        // 配列の長さに差があるのでindexを調整している
+        let ai = i - 1;
+        let bi = i - 2;
+        res.push((res[i - 1] + a[ai]).min(res[i - 2] + b[bi]));
+    }
+
+    let mut ans: Vec<usize> = vec![];
+    let mut idx = n - 1;
+    ans.push(idx + 1);
+    while idx > 0 {
+        // 配列の長さに差があるのでindexを調整している
+        let ai = idx - 1;
+        if res[idx] == (res[idx - 1] + a[ai]) {
+            idx -= 1;
+        } else {
+            idx -= 2;
+        }
+        ans.push(idx + 1);
+    }
+    ans.reverse();
+    println!("{:?}", ans.len());
+    println!("{}", ans.iter().join(" "));
 }

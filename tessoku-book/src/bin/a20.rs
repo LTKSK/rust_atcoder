c0b036667fe75959a9ed34269a245f14fef7c59f
LTKSK <ltksk0714@gmail.com>
2023-02-27 22:16:59 +0900

    a20

--- tessoku-book/src/bin/a20.rs
+++ tessoku-book/src/bin/a20.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+use proconio::marker::Chars;
+use std::cmp::max;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+        t: Chars
+    };
+
+    let mut dp = vec![vec![0; t.len() + 1]; s.len() + 1];
+    for (i, s) in s.iter().enumerate() {
+        for (j, t) in t.iter().enumerate() {
+            // 文字が同じだったら右下を+1
+            // それ以外は、上と左から値が大きいものをもらってくる
+            if t == s {
+                dp[i + 1][j + 1] = max(dp[i][j] + 1, max(dp[i + 1][j], dp[i][j + 1]));
+            } else {
+                dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1]);
+            }
+        }
+    }
+    println!("{}", dp[s.len()][t.len()])
 }

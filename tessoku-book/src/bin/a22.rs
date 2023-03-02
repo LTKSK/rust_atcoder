bbfa000adf78904c08c3d8d8913dd20cc900632c
LTKSK <ltksk0714@gmail.com>
2023-03-02 20:44:11 +0900

    a22

--- tessoku-book/src/bin/a22.rs
+++ tessoku-book/src/bin/a22.rs
@@ -1,3 +1,20 @@
+use proconio::input;
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n-1],
+        b: [usize; n-1],
+    };
+
+    let mut dp = vec![-std::isize::MAX; n + 1];
+    dp[1] = 0;
+
+    for i in 1..n {
+        let a = a[i - 1];
+        let b = b[i - 1];
+        dp[a] = dp[a].max(100 + dp[i]);
+        dp[b] = dp[b].max(150 + dp[i]);
+    }
+
+    println!("{}", dp[n]);
 }

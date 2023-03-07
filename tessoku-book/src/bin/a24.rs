6d010498fbd8c784aa0a10b7f756a00a0477329e
LTKSK <ltksk0714@gmail.com>
2023-03-07 22:00:30 +0900

    a24

--- tessoku-book/src/bin/a24.rs
+++ tessoku-book/src/bin/a24.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+use superslice::Ext;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n]
+    };
+
+    let mut ans = 0;
+    let mut dp = vec![std::usize::MAX; n + 1];
+    dp[0] = 0;
+    for a in a {
+        let pos = dp[0..=ans].lower_bound(&a);
+        if dp[pos] > a {
+            dp[pos] = a;
+        }
+        if pos > ans {
+            ans = pos;
+        }
+    }
+    println!("{}", ans);
 }

29eaffd5fda31084aef8bffe294f5159f05b2894
LTKSK <ltksk0714@gmail.com>
2023-03-08 22:36:42 +0900

    mod: comment

--- tessoku-book/src/bin/a24.rs
+++ tessoku-book/src/bin/a24.rs
@@ -11,10 +11,13 @@ fn main() {
     let mut dp = vec![std::usize::MAX; n + 1];
     dp[0] = 0;
     for a in a {
+        // aを挿入できる最大のindexがposとして返る
         let pos = dp[0..=ans].lower_bound(&a);
+        // dp[i]は最後の要素がaである部分列の最大の長さなので、dp[i]の要素よりaが小さかったら更新する
         if dp[pos] > a {
             dp[pos] = a;
         }
+        // 最大値が伸ばせる場合は更新
         if pos > ans {
             ans = pos;
         }

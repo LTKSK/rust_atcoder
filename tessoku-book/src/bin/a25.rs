84c63762f4ef2a1e703c7e5c4036bbba5e4bea3e
LTKSK <ltksk0714@gmail.com>
2023-03-13 22:55:41 +0900

    a25

--- tessoku-book/src/bin/a25.rs
+++ tessoku-book/src/bin/a25.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        h: usize,
+        w: usize,
+        c: [Chars; h]
+    };
+
+    let mut dp = vec![vec![0_usize; w]; h];
+    // 出発点に向かう方法は一通り
+    dp[0][0] = 1;
+
+    for x in 0..w {
+        for y in 0..h {
+            if x != 0 && c[y][x] != '#' {
+                dp[y][x] += dp[y][x - 1];
+            }
+            if y != 0 && c[y][x] != '#' {
+                dp[y][x] += dp[y - 1][x];
+            }
+        }
+    }
+
+    println!("{}", dp[h - 1][w - 1]);
 }

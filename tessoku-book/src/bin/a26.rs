fddbb438c35ee2ad7e3c3ee925b8fb472e13dd2b
LTKSK <ltksk0714@gmail.com>
2023-03-14 09:41:53 +0900

    a26

--- tessoku-book/src/bin/a26.rs
+++ tessoku-book/src/bin/a26.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
+fn is_prime(x: usize) -> bool {
+    let mut d = 2;
+    // 探索範囲は√x以下で十分
+    while d * d <= x {
+        if x % d == 0 {
+            return false;
+        }
+        d += 1;
+    }
+    return true;
+}
+
 fn main() {
-    todo!();
+    input! {
+        q: usize,
+        x: [usize; q]
+    };
+
+    for v in x {
+        if is_prime(v) {
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+    }
 }

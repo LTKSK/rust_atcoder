879fedfcedfbedbc846151dbf5acf942b38614ff
LTKSK <ltksk0714@gmail.com>
2023-03-17 09:50:53 +0900

    a27

--- tessoku-book/src/bin/a27.rs
+++ tessoku-book/src/bin/a27.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
+fn gcd(mut a: usize, mut b: usize) -> usize {
+    // どちらかが0になるまで続けて、0になった時のもう片方が答え
+    while a > 0 && b > 0 {
+        // 大きい値をmod取った値に更新する
+        if a >= b {
+            a = a % b;
+        } else {
+            b = b % a;
+        }
+    }
+    if a != 0 {
+        a
+    } else {
+        b
+    }
+}
+
 fn main() {
-    todo!();
+    input! {
+        a: usize,
+        b: usize,
+    };
+
+    println!("{}", gcd(a, b))
 }

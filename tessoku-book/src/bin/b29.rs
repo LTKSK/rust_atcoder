f497cf91ba5a81d7ad2220e4b62b4b62cf17db89
LTKSK <ltksk0714@gmail.com>
2023-03-22 23:21:30 +0900

    b29

--- tessoku-book/src/bin/b29.rs
+++ tessoku-book/src/bin/b29.rs
@@ -1,3 +1,19 @@
+use proconio::input;
 fn main() {
-    todo!();
+    input! {
+        a: usize,
+        b: usize,
+    };
+    let div = 1000000007;
+    let mut ans = 1;
+    let mut v = a;
+    for i in 0..60 {
+        let d = 1 << i;
+        if (b / d) % 2 == 1 {
+            ans = (ans * v) % div;
+        }
+        v = (v * v) % div;
+    }
+
+    println!("{}", ans);
 }
